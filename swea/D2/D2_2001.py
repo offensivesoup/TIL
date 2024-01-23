61c1d7c639b72030a367a5cc7211762cb68ad8eb
dhwan <ehdghks9719@naver.com>
2024-01-23 22:22:52 +0900

    코딩 포기할까

--- swea/D2/D2_2001.py
+++ swea/D2/D2_2001.py
@@ -1,15 +1,31 @@
 # N*N의 숫자는 해당 영역 안에 존재하는 파리의 개수
 # M*M의 크기의 파리채로 특정 영역을 선택했을때, 가장 max의 파리 수.
-
-area = [] # 사용자의 입력 영역 (파리수)
-paris = [] # 파리채 크기
+# 규칙성을 찾아보자
+# 5*5 에서 만약 파리채가 1*1 이라면? 25번(5**2) 2*2라면16번(4**2) 3*3 9번 (3**2)
+# 4*4라면 4번 2**2번 ...
+# 6*6에서도 마찬가지일까? 1*1이라면 6**2 2*2라면 5**2 ...
+## 어떤 사각형이든 파리채의 n - m + 1 의 제곱씩 움직인다.
 
 T = int(input())
 
 for i in range(1,T+1):
+    area = [] # 사용자의 입력 영역
+    paris = [] # 잡은 파리수 더해줄 영역
+    result = []
     N, M = map(int,input().split())
     # 사용자의 입력 영역 만들기
-    for row in range(N):
+    for user_input in range(N):
         area.append(list(map(int,input().split())))
-    
+    for i in range(N):
+        num = 0
+        selected_area = area[i:i+M]
+        for row in selected_area:
+            
+
+        # for j in range(N):
+        #     num += selected_area[i][:j+M]
+        #     num += area[]
+
+
+
 
