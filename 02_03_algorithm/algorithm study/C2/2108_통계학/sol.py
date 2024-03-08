0b3e4b00e3c07999e258fb2f3b2f640b19f3c495
김동환 <ehdghks9719@naver.com>
2024-03-08 17:34:44 +0900

    web03

--- 02_03_algorithm/algorithm study/C2/2108_통계학/sol.py
+++ 02_03_algorithm/algorithm study/C2/2108_통계학/sol.py
@@ -1,34 +1,28 @@
 import sys
 sys.stdin = open('input.txt')
+from collections import Counter
 sumi = 0
 N = int(input())
 numLst = [int(input()) for _ in range(N)]
-average = sum(numLst)/N
 # 산술평균
-if abs(average - int(average)) >= 0.5:
-    if average > 0:
-        average = int(average) + 1
-    else:
-        average = int(average) - 1
-else:
-    average = int(average)
+average = round(sum(numLst)/N)
 # 중위수
 mid = sorted(numLst)[N//2]
 # 범위
 length = len(range(min(numLst),max(numLst)))
 # 최빈값
 cnt = 0
-cntDict = {}
-for num in numLst:
-    cntDict[num] = numLst.count(num)
-resultLst = []
-for cnt in cntDict:
-    if cntDict[cnt] == max(cntDict.values()):
-        resultLst.append(cnt)
-if len(resultLst) == 1:
-    cnt = resultLst[0]
+c = Counter(numLst)
+most = c.most_common()
+maxi = most[0][1]
+result = []
+for num in most:
+    if num[1] == maxi:
+        result.append(num[0])
+if len(result) >1:
+    cnt = sorted(result)[1]
 else:
-    cnt = sorted(resultLst)[1]
+    cnt = result[0]
 
 print(average)
 print(mid)
