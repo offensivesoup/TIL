be13afe7db7588a7b39090766a640135293cb788
김동환 <ehdghks9719@naver.com>
2024-04-03 17:01:52 +0900

    디비 2일차 실습 4단계 제출

---
+++ 06_DB/02-01-many-to-one-relationship/online/articles/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Article, Comment
+from .forms import ArticleForm, CommentForm
+
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    # 특정 게시글에 작성된 모든 댓글 조회 (Article -> Comment, 역참조)
+    comments = article.comment_set.all()
+    comment_form = CommentForm()
+    context = {
+        'article': article,
+        'comments' : comments,
+        'comment_form' : comment_form
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/create.html', context)
+
+
+@login_required
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+
+@login_required
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'form': form,
+        'article': article,
+    }
+    return render(request, 'articles/update.html', context)
+
+def comments_create(request, pk):
+    # 게시글 조회
+    article = Article.objects.get(pk=pk)
+    comments = article.comment_set.all()
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment.save()
+        return redirect('articles:detail', article.pk)
+    context = {
+        'article' : article,
+        'comment_form' : comment_form,
+        'comments' : comments
+    }
+    return render(request, 'articles/detail.html', context)
+
+def comments_delete(request, article_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    # 아래 코드 처럼 작성 가능
+    # 단, 이렇게 작성할 경우 url 구조가 깨진다
+    # 지금까지의 url 전체 구성 및 통일성 유지 위해 아래 코드 방식을 선택 x
+    # article_pk = comment.article.pk
+    comment.delete()
+    return redirect('articles:detail', article_pk)
+
+    
